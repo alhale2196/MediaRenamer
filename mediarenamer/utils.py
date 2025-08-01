01e7e87a472717aa73328dded062e08c494852d3
Andrew Hale <halea2196@gmail.com>
2025-07-31 20:51:14 -0400

    Add season number parser and episode number parser

--- mediarenamer/utils.py
+++ mediarenamer/utils.py
@@ -1,13 +1,36 @@
 import os
 import sys
 import json
+import re
 from typing import Optional, List
 
 try:
     from .exceptions import FileException
 except ImportError:
     sys.path.append('./')
-    from exceptions import FileException
+    from exceptions import MediaRenamerException, FileException, ParserException
+
+try:
+    from .file_utils import (
+    write_to_file,
+    get_current_directory_basename,
+    get_list_of_folders_in_directory,
+    get_list_of_files_in_directory,
+    rename_file,
+    rename_directory,
+    get_file_extension,
+    )
+except ImportError:
+    sys.path.append('./')
+    from file_utils import (
+        write_to_file,
+        get_current_directory_basename,
+        get_list_of_folders_in_directory,
+        get_list_of_files_in_directory,
+        rename_file,
+        rename_directory,
+        get_file_extension,
+    )
 
 
 def get_season_number_from_folder_name(folder_name: str) -> Optional[int]:
@@ -18,6 +41,13 @@ def get_season_number_from_folder_name(folder_name: str) -> Optional[int]:
 
     :return: The season number.
     """
+    try:
+        match = re.search(r'(?:Season\s*|S)(\d+)', folder_name, re.IGNORECASE)
+        if match:
+            return int(match.group(1))
+        return None
+    except Exception as e:
+        raise ParserException(str(e))
 
 def get_episode_number_from_file_name(file_name: str) -> Optional[int]:
     """
@@ -27,3 +57,10 @@ def get_episode_number_from_file_name(file_name: str) -> Optional[int]:
 
     :return: The episode number.
     """
+    try:
+        match = re.search(r'(?:Episode\s*|E|Part\s*)(\d+)', file_name, re.IGNORECASE)
+        if match:
+            return int(match.group(1))
+        return None
+    except Exception as e:
+        raise ParserException(str(e))
